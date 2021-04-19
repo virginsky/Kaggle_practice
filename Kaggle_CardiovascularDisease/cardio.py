e11c865c8ccf8552b813e0861bcb46041323bb36
virginsky <77185864+virginsky@users.noreply.github.com>
2021-04-19 17:50:13 +0900

    21.04.19 done(XGboosting)

--- Kaggle_CardiovascularDisease/cardio.py
+++ Kaggle_CardiovascularDisease/cardio.py
@@ -16,6 +16,7 @@ from sklearn import metrics
 import sys
 import os
 import statistics
+
 dir = os.path.dirname(os.path.realpath(__file__))
 
 df = pd.read_csv(dir+'./cardio_train.csv', sep= ';')
@@ -124,7 +125,15 @@ max_bmi = Q3 + 1.5*IQR
 # print(max_bmi)
 df0['BMI'] = np.where(df0['bmi'] < min_bmi, int(df0['bmi'].mean()), (np.where(df0['bmi'] > max_bmi, int(df0['bmi'].mean()), df0['bmi'])))
 # print(df0.describe())
+df0.loc[df0['BMI'] <= 18.5, 'BMI_bin'] = 0
+df0.loc[(df0['BMI'] > 18.5) & (df0['BMI'] <=23), 'BMI_bin'] = 1
+df0.loc[(df0['BMI'] > 23) & (df0['BMI'] <=25), 'BMI_bin'] = 2
+df0.loc[(df0['BMI'] > 25) & (df0['BMI'] <=30), 'BMI_bin'] = 3
+df0.loc[(df0['BMI'] > 30) & (df0['BMI'] <=35), 'BMI_bin'] = 4
+df0.loc[df0['BMI'] > 35, 'BMI_bin'] = 5
+
 df0=df0.drop(columns = 'bmi')
+df0=df0.drop(columns = 'BMI')
 
 # #새로생성한 변수들 그래프 확인
 # figure, ( (ax1, ax2),(ax3,ax4)  ) = plt.subplots(nrows=2, ncols=2)
@@ -149,6 +158,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 # print(X_test.shape)
 
 
+
 # DecisionTree Classifier 생성 및 학습
 dt_clf = DecisionTreeClassifier(max_depth=10,random_state=0)
 dt_clf.fit(X_train , y_train)
@@ -156,15 +166,11 @@ print("DecisionTree Accuracy on training set: {:.3f}".format(dt_clf.score(X_trai
 print("DecisionTree Accuracy on test set: {:.3f}".format(dt_clf.score(X_test, y_test)))
 
 
-
 # randomforest 분석
 rf = RandomForestClassifier(n_estimators=100)
 rf.fit(X_train, y_train)
 rf_pre=rf.predict(X_test)
 print("RandomForest score: ",rf.score(X_test,y_test))
-
-
-
 
 
 # Ada boosting
@@ -178,3 +184,73 @@ model = abc.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 # Model Accuracy, how often is the classifier correct?
 print("Ada boosting Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
+# XG boosting (XGBClassfier?)
+      # https://wooono.tistory.com/97     - 설명참고
+      # https://injo.tistory.com/44       - 모델세팅 참고
+
+from xgboost import XGBClassifier
+import xgboost as xgb
+
+# 넘파이 형태의 학습 데이터 세트와 테스트 데이터를 DMatrix로 변환하는 예제
+dtrain = xgb.DMatrix(data=X_train, label = y_train)
+dtest = xgb.DMatrix(data=X_test, label=y_test)
+
+# max_depth = 3, 학습률은 0.1, 예제가 이진분류이므로 목적함수(objective)는 binary:logistic(이진 로지스틱)
+# 오류함수의 평가성능지표는 logloss
+# 부스팅 반복횟수는 400
+# 조기중단을 위한 최소 반복횟수는 100
+
+params = {'booster' : 'gbtree', 'max_depth' : 6,
+         'eta' : 0.1, 
+         'objective' : 'binary:logistic',
+         'eval_metric' : 'logloss',
+         'early_stoppings' : 100, 'lambda':10, 'colsample_bytree' : 0.5}
+
+num_rounds = 400
+
+# train 데이터 세트는 'train', evaluation(test) 데이터 세트는 'eval' 로 명기
+wlist = [(dtrain, 'train'), (dtest,'eval')]
+# 하이퍼 파라미터와 early stopping 파라미터를 train() 함수의 파라미터로 전달
+xgb_model = xgb.train(params = params, dtrain=dtrain, num_boost_round=num_rounds, evals=wlist)
+
+pred_probs = xgb_model.predict(dtest)
+print('predict() 수행 결과값을 10개만 표시, 예측 확률 값으로 표시됨')
+print(np.round(pred_probs[:10], 3))
+
+# 예측 확률이 0.5보다 크면 1, 그렇지 않으면 0으로 예측값 결정해 리스트 객체인 preds에 저장
+preds = [ 1 if x > 0.5 else 0 for x in pred_probs]
+print('예측값 10개만 표시: ', preds[:10])
+
+# 혼동행렬, 정확도, 정밀도, 재현율, F1, AUC 불러오기
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score
+import warnings
+def get_clf_eval(y_test, y_pred):
+    confusion = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    F1 = f1_score(y_test, y_pred)
+    AUC = roc_auc_score(y_test, y_pred)
+    print('오차행렬:\n', confusion)
+    print('\n정확도: {:.4f}'.format(accuracy))
+    print('정밀도: {:.4f}'.format(precision))
+    print('재현율: {:.4f}'.format(recall))
+    print('F1: {:.4f}'.format(F1))
+    print('AUC: {:.4f}'.format(AUC))
+
+get_clf_eval(y_test, preds)
+
+from xgboost import plot_importance
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(10, 12))
+plot_importance(xgb_model, ax=ax)
+
+xgb.plot_tree(xgb_model, num_trees=0, rankdir='LR')
+fig = plt.gcf()
+fig.set_size_inches(150, 100)
+
+plt.show()
